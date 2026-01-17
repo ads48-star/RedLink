bd2f369c05f5d4bb65ba187b3484f542572331d1
ads48-star <adhvika.srinath@gmail.com>
2026-01-17 08:15:38 +0530

    Update User.js

--- redLink-backend/models/User.js
+++ redLink-backend/models/User.js
@@ -1,1 +1,17 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  name: String,
+  email: { type: String, unique: true },
+  password: String,
+  bloodGroup: String,
+  role: { type: String, enum: ["donor", "hospital", "admin"], default: "donor" },
+  location: {
+    lat: Number,
+    lng: Number
+  },
+  lastDonation: Date
+});
+
+module.exports = mongoose.model("User", UserSchema);
 
