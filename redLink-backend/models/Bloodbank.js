2252971d721da42e55938ca22497c4610343d7c4
ads48-star <adhvika.srinath@gmail.com>
2026-01-17 08:15:18 +0530

    Update Bloodbank.js

--- redLink-backend/models/Bloodbank.js
+++ redLink-backend/models/Bloodbank.js
@@ -1,1 +1,22 @@
+const mongoose = require("mongoose");
+
+const BloodBankSchema = new mongoose.Schema({
+  name: String,
+  location: {
+    lat: Number,
+    lng: Number
+  },
+  inventory: {
+    "A+": Number,
+    "A-": Number,
+    "B+": Number,
+    "B-": Number,
+    "O+": Number,
+    "O-": Number,
+    "AB+": Number,
+    "AB-": Number
+  }
+});
+
+module.exports = mongoose.model("BloodBank", BloodBankSchema);
 
