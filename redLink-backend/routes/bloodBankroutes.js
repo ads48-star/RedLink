226573449315c7634c42a421f027c56cb23b951a
ads48-star <adhvika.srinath@gmail.com>
2026-01-17 08:17:00 +0530

    Update bloodBankroutes.js

--- redLink-backend/routes/bloodBankroutes.js
+++ redLink-backend/routes/bloodBankroutes.js
@@ -1,1 +1,23 @@
+const express = require("express");
+const BloodBank = require("../models/BloodBank");
+
+const router = express.Router();
+
+// Get nearby blood banks (basic)
+router.get("/", async (req, res) => {
+  const bloodBanks = await BloodBank.find();
+  res.json(bloodBanks);
+});
+
+// Update blood inventory
+router.put("/:id", async (req, res) => {
+  const updated = await BloodBank.findByIdAndUpdate(
+    req.params.id,
+    { inventory: req.body.inventory },
+    { new: true }
+  );
+  res.json(updated);
+});
+
+module.exports = router;
 
